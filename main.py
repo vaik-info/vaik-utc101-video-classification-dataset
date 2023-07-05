70be31027ae744ee920cdb8df3fd55b21c03a7c0
k-wakisaka <k.wakisaka@vaikobo.com>
2023-07-05 09:04:13 +0900

    first commit

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 import tensorflow_datasets as tfds
 
 
-def dump(split, classes_txt_path, image_height, image_width, fps, quality, max_class_num, output_dir_path):
+def dump(split, classes_txt_path, image_height, image_width, fps, max_class_num, output_dir_path):
     output_split_dir_path = os.path.join(output_dir_path, split)
 
     os.makedirs(output_split_dir_path, exist_ok=True)
@@ -32,8 +32,7 @@ def dump(split, classes_txt_path, image_height, image_width, fps, quality, max_c
         ratio = min(image_height / video.shape[1], image_width / video.shape[2])
         video = tf.image.resize(video, (round(ratio * video.shape[1]), round(ratio * video.shape[2])))
         image_array_list = [np.clip(image.numpy(), 0.0, 255.0).astype(np.uint8) for image in video]
-        imageio.mimwrite(os.path.join(output_sub_dir_path, f'{class_label}_{data_index}.mp4'), image_array_list,
-                         fps=fps, quality=quality, codec='libx264')
+        imageio.mimwrite(os.path.join(output_sub_dir_path, f'{class_label}_{data_index}.avi'), image_array_list, fps=fps, codec='rawvideo')
 
     with open(os.path.join(output_dir_path, os.path.basename(classes_txt_path)), 'w') as f:
         for class_label in classes:
@@ -42,13 +41,12 @@ def dump(split, classes_txt_path, image_height, image_width, fps, quality, max_c
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='dump')
-    parser.add_argument('--split', type=str, default='test')
+    parser.add_argument('--split', type=str, default='train')
     parser.add_argument('--classes_txt_path', type=str,
                         default=os.path.join(os.path.dirname(__file__), 'ucf101_labels.txt'))
     parser.add_argument('--image_height', type=int, default=224)
     parser.add_argument('--image_width', type=int, default=224)
     parser.add_argument('--fps', type=int, default=25)
-    parser.add_argument('--quality', type=int, default=10)
     parser.add_argument('--max_class_num', type=int, default=25)
     parser.add_argument('--output_dir_path', type=str, default='~/.vaik-utc101-video-classification-dataset')
 
@@ -56,5 +54,5 @@ if __name__ == '__main__':
     args.classes_txt_path = os.path.expanduser(args.classes_txt_path)
     args.output_dir_path = os.path.expanduser(args.output_dir_path)
 
-    dump(args.split, args.classes_txt_path, args.image_height, args.image_width, args.fps, args.quality,
+    dump(args.split, args.classes_txt_path, args.image_height, args.image_width, args.fps,
          args.max_class_num, args.output_dir_path)
